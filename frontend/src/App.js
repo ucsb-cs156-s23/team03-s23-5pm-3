f35648953bf97105e1453e4dab01bde33fda3e8b
Bryan Olivares <bolivares@umail.ucsb.edu>
2023-05-18 14:46:47 -0700

    added nav and app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,11 @@ import TodosIndexPage from "main/pages/Todos/TodosIndexPage";
 import TodosCreatePage from "main/pages/Todos/TodosCreatePage";
 import TodosEditPage from "main/pages/Todos/TodosEditPage";
 
+import RestaurantCreatePage from "main/pages/Restaurants/RestaurantCreatePage";
+import RestaurantEditPage from "main/pages/Restaurants/RestaurantEditPage";
+import RestaurantIndexPage from "main/pages/Restaurants/RestaurantIndexPage";
+import RestaurantDetailsPage from "main/pages/Restaurants/RestaurantDetailsPage";
+
 import UCSBDatesIndexPage from "main/pages/UCSBDates/UCSBDatesIndexPage";
 import UCSBDatesCreatePage from "main/pages/UCSBDates/UCSBDatesCreatePage";
 import UCSBDatesEditPage from "main/pages/UCSBDates/UCSBDatesEditPage";
@@ -35,6 +40,10 @@ function App() {
               <Route exact path="/todos/list" element={<TodosIndexPage />} />
               <Route exact path="/todos/create" element={<TodosCreatePage />} />
               <Route exact path="/todos/edit/:todoId" element={<TodosEditPage />} />
+
+              
+              <Route exact path="/restaurants/details/:id" element={<RestaurantDetailsPage />} />
+              
             </>
           )
         }
@@ -43,6 +52,7 @@ function App() {
           hasRole(currentUser, "ROLE_USER") && (
             <>
               <Route exact path="/ucsbdates/list" element={<UCSBDatesIndexPage />} />
+              <Route exact path="/restaurants/" element={<RestaurantIndexPage />} />
             </>
           )
         }
@@ -51,6 +61,9 @@ function App() {
             <>
               <Route exact path="/ucsbdates/edit/:id" element={<UCSBDatesEditPage />} />
               <Route exact path="/ucsbdates/create" element={<UCSBDatesCreatePage />} />
+
+              <Route exact path="/restaurants/create" element={<RestaurantCreatePage />} />
+              <Route exact path="/restaurants/edit/:id" element={<RestaurantEditPage />} />
             </>
           )
         }
